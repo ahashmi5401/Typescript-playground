dc52ea41203dbb5242e25c1c950282a7948bceba
ahashmi5401 <ahashmi5401@gmail.com>
2026-01-17 14:13:16 +0500

    Add 01-typescript as normal folder

---
+++ 01-typescript/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ts = 4;
+console.log(`TypeScript timestamp: ${ts}`);
+function add(a, b) {
+    return a + b;
+}
+let added = add(2, 4);
+console.log(added);
+//# sourceMappingURL=App.js.map
---
+++ 01-typescript/App.ts
@@ -0,0 +1,8 @@
+let ts = 4;
+console.log(`TypeScript timestamp: ${ts}`);
+
+function add(a:number, b:number):number {
+    return a + b;
+}
+let added = add(2,4)
+console.log(added);
