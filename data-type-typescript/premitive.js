9d6580fffa81e0f610f629dd5c91804c3f93a539
ahashmi5401 <ahashmi5401@gmail.com>
2026-01-17 14:32:55 +0500

    Data Type in Type Script

---
+++ data-type-typescript/premitive.js
@@ -0,0 +1,8 @@
+console.log("================>TypeScript Primitive Data Types Example<================");
+var numberVar = 10;
+var stringVar = "Hello, TypeScript!";
+var booleanVar = true;
+
+console.log("Number:", typeof numberVar);
+console.log("String:", typeof stringVar);
+console.log("Boolean:", typeof booleanVar);
---
+++ data-type-typescript/premitive.ts
@@ -0,0 +1,9 @@
+console.log("================>TypeScript Primitive Data Types Example<================");
+let numberVar : number = 10;
+let stringVar : string = "Hello, TypeScript!";
+let booleanVar : boolean = true;
+
+console.log("Number:", typeof numberVar);
+console.log("String:", typeof stringVar);
+console.log("Boolean:", typeof booleanVar);
+
